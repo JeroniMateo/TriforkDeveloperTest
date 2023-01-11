59bbe9b22a2471e31677671367d4edca1a01a5bd
Jeroni <jmateo@cifpfbmoll.eu>
2023-01-12 00:15:07 +0100

    OctoKit Login Authetication

--- Octokit.js
+++ Octokit.js
@@ -1,7 +1,21 @@
+import { Octokit, App } from 'octokit'
 
+const octokit = new Octokit({
+  auth: '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'
+})
 
+await octokit.request('GET /octocat', {})
 
-const octokit = new Octokit({ 
-    auth: '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',
-});
-await octokit.request("GET /octocat", {});
+await octokit.rest.issues.create({
+  owner: 'octocat',
+  repo: 'hello-world',
+  title: 'Hello, world!',
+  body: 'I created this issue using Octokit!'
+})
+
+await octokit.request('POST /repos/{owner}/{repo}/issues', {
+  owner: 'octocat',
+  repo: 'hello-world',
+  title: 'Hello, world!',
+  body: 'I created this issue using Octokit!'
+})
