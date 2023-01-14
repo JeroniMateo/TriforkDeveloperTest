5d5ab27018fdef76258ae616747e9d85b5074bd2
Jeroni <jmateo@cifpfbmoll.eu>
2023-01-14 20:33:59 +0100

    Organization Input

--- GitHubApi.js
+++ GitHubApi.js
@@ -8,38 +8,42 @@ We can get the repos we have, our name, followers among others.
 /* Then we create an other fetch of our repositories to get the data that we need */
 
 let token = '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'
-const repos = 'https://api.github.com/orgs/errfree/repos'
-fetch(repos, {
+let organization = document.getElementById('OrgazinationInput').value;
+function getOrganization() {
+  const repos = `https://api.github.com/orgs/${organization}/repos`
+  
+  
+  fetch(repos, {
   headers: {
     Accept: 'application/vnd.github.v3+json',
     Authorization: `token ${token}`
   }
 })
-  .then((response) => response.json()) //Converting the response to a JSON object
-  /* The Data that we have when we make the fetch is an array, 
+.then((response) => response.json()) //Converting the response to a JSON object
+/* The Data that we have when we make the fetch is an array, 
 so to know how many repos we have we want to know how long is it, where every position of the array is 1 repo */
   .then((data) => {
     console.log(data)
     const root = document.querySelector('#ReposNum')
-
+    
     root.innerHTML = ` 
     
     <p><label>Number of Repositories:</label> ${data.length}</p>
     `
-
-   
+    
+    
   })
-
-  .catch((error) => console.error(error))
-
-// Init - Contains request body and headers and other configurations such as the request method.
-
-// TODO: 2. Given an organization return the biggest repository (in bytes).
-
-
-
   
-  fetch(`https://api.github.com/orgs/engineyard/repos`, {
+  .catch((error) => console.error(error))
+  
+  // Init - Contains request body and headers and other configurations such as the request method.
+  
+  // TODO: 2. Given an organization return the biggest repository (in bytes).
+  
+  
+  
+  
+  fetch(`https://api.github.com/orgs/${organization}/repos`, {
     headers: {
       Accept: 'application/vnd.github.v3+json',
       Authorization: `token ${token}`
@@ -60,44 +64,46 @@ so to know how many repos we have we want to know how long is it, where every po
     }
     console.log(ReposSyze)
     
-      root.innerHTML += ` 
-      <p><label>Biggest Repo:</label> ${ReposSyze.max()} <small>bytes</small></p>
-      
-      `
-      
+    root.innerHTML += ` 
+    <p><label>Biggest Repo:</label> ${ReposSyze.max()} <small>bytes</small></p>
+    
+    `
+    
   })
   .catch((error) => console.error(error))
   
-
-// TODO: 3. Return the number of organizations that are currently on Github.
-
-/**If we have the array of the git hub organizations, to know how many they are, we have to do the same as we do on the first TODO;
- * we want to know the length of the array where every position of the array is 1 oranization */
-
-fetch('https://api.github.com/organizations', {
-  headers: {
-    Accept: 'application/vnd.github.v3+json',
-    Authorization: `token ${token}`
-  }
-})
+  
+  // TODO: 3. Return the number of organizations that are currently on Github.
+  
+  /**If we have the array of the git hub organizations, to know how many they are, we have to do the same as we do on the first TODO;
+   * we want to know the length of the array where every position of the array is 1 oranization */
+  
+}
+  fetch('https://api.github.com/organizations', {
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${token}`
+    }
+  })
   .then((response) => response.json()) //Converting the response to a JSON object
   /* The Data that we have when we make the fetch is an array, 
 so to know how many repos we have we want to know how long is it, becouse every position of the array is 1 repo */
-  .then((data) => {
-    console.log(data)
-    const root = document.querySelector('#OrganizationsNum')
-
-    root.innerHTML = ` 
-    
-    <p><label>Number of Organizations:</label> ${data.length}</p>
-    
-    `
-
-    for (let i = 0; i < data.length; i++) {
-      document.getElementById(
-        'OrganizationsList'
+.then((data) => {
+  console.log(data)
+  const root = document.querySelector('#OrganizationsNum')
+  
+  root.innerHTML = ` 
+  
+  <p><label>Number of Organizations:</label> ${data.length}</p>
+  
+  `
+  
+  for (let i = 0; i < data.length; i++) {
+    document.getElementById(
+      'OrganizationsList'
       ).innerHTML += `<a href='${data[i].url}'><li>${data[i].login}</li></a>`
     }
   })
-
+  
   .catch((error) => console.error(error))
+  
